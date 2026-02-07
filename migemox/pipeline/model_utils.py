4b105014ccdd295632a272aa1e983c5a0585432b
Nicholas Yousefi <nsyousef@ucsd.edu>
2026-02-06 23:34:28 -0500

    fix bug where could not find rxn id due to missing model name

--- migemox/pipeline/model_utils.py
+++ migemox/pipeline/model_utils.py
@@ -15,32 +15,22 @@ def find_biomass_candidates(
     Find candidate reactions that could be the biomass reaction.
 
     By default, or if `microbe_name = None` this function searches the model for reactions whose ID
-    starts with 'bio' (case insensitive). This mirrors the behavior of MMT.
+    starts with 'bio' (case sensitive). This mirrors the behavior of MMT.
 
     However, if the reactions in a microbe are tagged with the microbe's name, then this will break
-    that functionality, as the biomass reaction ID will no longer start with `bio`. To fix this, you will
-    need to pass in the `microbe_name` if that is the case. If a `microbe_name` is passed in, it will be removed
-    from any reaction in which it is present before checking if that reaction starts with `bio`. Please note that this
-    removal is case insensitive.
+    that functionality, as the biomass reaction ID will start with the microbe name instead of `bio`.
+    To work around this issue, if the reaction IDs are tagged with the microbe name, you must pass in
+    the microbe name in the `microbe_name` parameter. Please note that this should just be the microbe
+    name itself (e.g. E_coli) **without** a trailing underscore (i.e. not E_coli_). This `microbe_name`
+    (plus a trailing underscore) will be removed from any reaction ID in which it is present prior to 
+    checking if the reaction id starts with `'bio'`. Please note that this check and removal is 
+    **case sensitive**.
 
-    Please note that this function will automatically concatenate an `_` to the end of `microbe_name` before removing the name.
-    This is because MiGEMox by convention puts an underscore between the microbe name and the reaction id. For example:
-
-    ```
-    microbe_name: E_coli
-    biomass_rxn_id: biomass123
-
-    tagged_rxn_name: E_coli_biomass_123
-                           ^ notice this underscore
-    ```
-
-    Therefore, you should not include this underscore in `microbe_name`. For example, set `microbe_name` to `E_coli` in the
-    case above. Do not set it to `E_coli_`.
-
-    **Another note:** Previously, MiGEMox searched for biomass reactions as reactions containing the word `'biomass'` in them. However,
-    this is insufficient as some AGORA2 models have biomass reactions named `bio1`. It is also insufficient to just search for reactions
-    whose IDs contain the string `'bio'` in them because there are reactions such as `'pbiosynthesis'` which are not the biomass reaction
-    and would therefore get wrongly flagged. For that reason, we must search specifically for srings that start with `'bio'`. This is
+    **Another note:** Previously, MiGEMox searched for biomass reactions as reactions containing the word 
+    `'biomass'` in them. However, this was insufficient as some AGORA2 models have biomass reactions named 
+    `bio1`. It is also insufficient to just search for reactions whose IDs contain the string `'bio'` in them 
+    because there are reactions such as `'pbiosynthesis'` which are not the biomass reaction and would therefore 
+    get wrongly flagged. For that reason, we must search specifically for srings that start with `'bio'`. This is
     the behavior MMT uses.
     
     :param model: The model or structural model to search to find biomass reaction candidates.
@@ -50,16 +40,18 @@ def find_biomass_candidates(
     :return: A list of reaction objects corresponding to potential biomass reactions.
     """
 
-    # get all reaction IDs in model
-    rxn_ids = [rxn.id.lower() for rxn in model.reactions]
+    biomass_candidate_rxns = []
+    for rxn in model.reactions:
 
-    # if necessary, remove microbe name (case insensitive)
-    if microbe_name is not None:
-        rxn_ids = [rxn_id.replace(microbe_name.lower() + '_', '') for rxn_id in rxn_ids]
+        # copy rxn id so don't override original
+        rxn_id = rxn.id
 
-    # find reaction IDs that start with 'bio'
-    biomass_candidate_ids = [rxn_id for rxn_id in rxn_ids if rxn_id.startswith("bio")]
+        # remove microbe_name from rxn_id if present
+        if microbe_name is not None:
+            rxn_id = rxn_id.replace(microbe_name + '_', '')
 
-    biomass_candidate_rxns = [model.reactions.get_by_id(rxn_id) for rxn_id in biomass_candidate_ids]
+        # check if it is a biomass reaction (case sensitive)
+        if rxn_id.startswith('bio'):
+            biomass_candidate_rxns.append(rxn)
 
     return biomass_candidate_rxns
